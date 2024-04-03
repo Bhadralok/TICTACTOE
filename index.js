5f9c9a778bb079b715b8d66ad5d2c795213a26a8
Bhadralok <105461298+Bhadralok@users.noreply.github.com>
2024-04-03 15:13:10 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let newGameButton = document.getElementById("newGame");
+let multiplayer = document.getElementById("multi");
+
+newGameButton.addEventListener("click", () => {
+    newGameButton.style.color = "red";
+})
