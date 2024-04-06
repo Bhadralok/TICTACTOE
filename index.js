8b35670dc9278926b5aae48126ba94154025b773
Bhadralok <105461298+Bhadralok@users.noreply.github.com>
2024-04-06 14:52:29 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let newGame = document.getElementById('newGame');
+newGame.addEventListener('click', () => {
+    localStorage.clear();
+})
+let multiButton = document.getElementById('multi');
+multiButton.addEventListener('click', () => {
+    alert('Coming Soon...');
+})
+let settingButton = document.getElementById('setting');
+settingButton.addEventListener('click', () => {
+    alert("Nothing Yet");
+})
